886f81ed580aff0649cecbc31a7ccc4a5f4e692d
Faiz Surani <faiz.surani@gmail.com>
2020-06-21 22:41:31 -0700

    fs - Added tests for image functionality

--- test/controllers/images_controller_test.rb
+++ test/controllers/images_controller_test.rb
@@ -2,47 +2,61 @@ require 'test_helper'
 
 class ImagesControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @image = images(:one)
+    @valid_image_url = images(:valid_image_url)
+    @wrong_url_type = images(:wrong_url_type)
+    @not_url = images(:not_url)
   end
 
-  test 'should get index' do
-    get images_url
-    assert_response :success
-  end
+  # test 'should get index' do
+  #   get images_url
+  #   assert_response :success
+  # end
 
   test 'should get new' do
     get new_image_url
     assert_response :success
   end
 
-  test 'should create image' do
-    assert_difference('Image.count') do
-      post images_url, params: { image: {} }
+  test 'should create image for valid url' do
+    assert_difference('Image.count', 1) do
+      post images_url, params: { image: @valid_image_url.as_json }
     end
-
     assert_redirected_to image_url(Image.last)
   end
 
-  test 'should show image' do
-    get image_url(@image)
-    assert_response :success
-  end
-
-  test 'should get edit' do
-    get edit_image_url(@image)
-    assert_response :success
-  end
-
-  test 'should update image' do
-    patch image_url(@image), params: { image: {} }
-    assert_redirected_to image_url(@image)
-  end
-
-  test 'should destroy image' do
-    assert_difference('Image.count', -1) do
-      delete image_url(@image)
+  test 'should not create image for non http/s url type' do
+    assert_no_difference('Image.count') do
+      post images_url, params: { image: @wrong_url_type.as_json }
     end
-
-    assert_redirected_to images_url
   end
+
+  test 'should not create image for non url' do
+    assert_no_difference('Image.count') do
+      post images_url, params: { image: @not_url.as_json }
+    end
+  end
+
+  test 'should show image' do
+    get image_url(@valid_image_url)
+    assert_response :success
+    assert_select 'img[alt="' + @valid_image_url.url + '"]'
+  end
+
+  #   test 'should get edit' do
+  #     get edit_image_url(@image)
+  #     assert_response :success
+  #   end
+  #
+  #   test 'should update image' do
+  #     patch image_url(@valid_image_url), params: { image: {} }
+  #     assert_redirected_to image_url(@image)
+  #   end
+  #
+  # test 'should destroy image' do
+  #   assert_difference('Image.count', -1) do
+  #     delete image_url(@valid_image_url)
+  #   end
+  #
+  #   assert_redirected_to images_url
+  # end
 end
