e25937fe49b1416db6b55176fbd5ab2be96315d6
Faiz Surani <faiz.surani@gmail.com>
2020-06-21 22:59:57 -0700

    fs - Add image index

--- test/controllers/images_controller_test.rb
+++ test/controllers/images_controller_test.rb
@@ -7,10 +7,10 @@ class ImagesControllerTest < ActionDispatch::IntegrationTest
     @not_url = images(:not_url)
   end
 
-  # test 'should get index' do
-  #   get images_url
-  #   assert_response :success
-  # end
+  test 'should get index' do
+    get images_url
+    assert_response :success
+  end
 
   test 'should get new' do
     get new_image_url
