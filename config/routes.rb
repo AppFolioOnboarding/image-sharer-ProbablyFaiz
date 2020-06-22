cd5bad44a21a135f4fca98648461f578b70a1b0b
ProbablyFaiz <faiz.surani@gmail.com>
2020-06-21 18:14:34 -0700

    fs - Resolve rubocop issues with generated files

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
-  resources :images, :only => [:new, :show]
+  resources :images, only: %i[create show]
 end
