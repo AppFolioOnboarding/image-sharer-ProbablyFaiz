2d1124df706cadf311c38431797c49c02cd92cbf
Faiz Surani <faiz.surani@appfolio.com>
2020-07-07 10:47:32 -0700

    fs - Add controller action for feedback

--- app/controllers/api/feedbacks_controller.rb
+++ app/controllers/api/feedbacks_controller.rb
@@ -1,7 +1,19 @@
 module Api
   class FeedbacksController < ApplicationController
     def create
-      # Implement your create action
+      @feedback = Feedback.new(feedback_params)
+
+      if @feedback.save
+        render json: { message: 'Feedback successfully sent.' }, status: :ok
+      else
+        render json: { message: @feedback.errors.full_messages.join(', ') }, status: :unprocessable_entity
+      end
+    end
+
+    private
+
+    def feedback_params
+      params.require(:feedback).permit(:name, :message)
     end
   end
 end
